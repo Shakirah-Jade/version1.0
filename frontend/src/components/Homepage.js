e33d02c25d6245ce0cd89c2b4b5b879df90627cf
unknown <admin@ICT-PC4.trobank.com>
2023-09-18 17:32:36 +0300

    with the share component version1.0

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import "../styles/homepage.css";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+// import Autosuggest from 'react-autosuggest';
 
-function Homepage() {
+function Homepage({ onClose }) {
   const [selectedFile, setSelectedFile] = useState(null);
   const [selectedFileName, setSelectedFileName] = useState("");
   const [notification, setNotification] = useState("");
@@ -20,7 +23,56 @@ function Homepage() {
       setNotification(`File "${uploadedFileName}" uploaded successfully!`);
     }
   };
+///////////////////////sharing logic ////////////////////////////
 
+const [showModal, setShowModal] = useState(false);
+const [recipient, setRecipient] = useState('');
+
+const openModal = () => {
+  setShowModal(true);
+};
+
+const closeModal = () => {
+  setShowModal(false);
+};
+const [content, setContent] = useState('');
+const handleShare = (recipient, content) => {
+  // Implement sharing logic here
+  console.log(`Sharing with: ${recipient}`);
+  closeModal(); // Close the modal after sharing
+
+   // Implement sharing logic here, e.g., send notifications to the recipient.
+   if (recipient && content) {
+    // Simulate sharing success for demonstration purposes
+    toast.success(`Shared with: ${recipient}`);
+    onClose(); // Close the modal after sharing
+  } else {
+    // Show an error message if recipient or content is missing
+    toast.error('Please enter both recipient and content.');
+  }
+};
+
+toast.success(`Shared with: ${recipient}`, {
+  position: toast.POSITION.BOTTOM_RIGHT,
+});
+
+
+// const [value, setValue] = useState('');
+//   const [suggestions, setSuggestions] = useState([]);
+
+//   const handleInputChange = (event, { newValue }) => {
+//     setValue(newValue);
+//   };
+
+  // const getSuggestions = (value) => {
+  //   Implement logic to fetch email/username suggestions here.
+  //   For example, filter from a list of existing users.
+  //   const suggestions = existingUsers.filter((user) =>
+  //     user.toLowerCase().includes(value.toLowerCase())
+  //   );
+  //   return suggestions;
+  // };
+/////////////////////////////////////////////////
   return (
     <div className="pagehome">
       <div className="background-image">
@@ -45,6 +97,37 @@ function Homepage() {
             Upload
           </button>
           {notification && <p className="notification">{notification}</p>}
+         {/* /////////////////// share component /////////////////////////////// */}
+          <div className="share">  
+          <button onClick={openModal}>Share</button>
+          {showModal && (
+            <div className="modal">
+              <div className="modal-content">
+              <span className="close" onClose={closeModal} onShare={handleShare}>
+              &times;
+             </span>
+             
+                <h2>Share Content</h2>
+                <input
+                  type="text"
+                  placeholder="Enter recipient's username or email"
+                  value={recipient}
+                  onChange={(e) => setRecipient(e.target.value)}
+                />
+                    <textarea
+              placeholder="Enter content to share"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            ></textarea>
+                <button onClick={handleShare}>Share</button>
+                <button onClick={closeModal}>Cancel</button>
+              </div>
+            </div>
+            
+          )}
+          <ToastContainer limit={1} />
+        </div>
+        
         </div>
       </div>
     </div>
